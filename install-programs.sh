53ba9b63b3c9c5617d2275e86c41920d1fa785ef
Adam Trimble <adam.trimble@gmail.com>
2019-04-29 16:32:03 -0700

    start molding dotfiles to GOAT

--- install-programs.sh
+++ install-programs.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Install command-line tools using Homebrew
+# Install xcode command-line tools
+xcode-select --install
 
 # Make sure we’re using the latest Homebrew
 brew update
 
 # Upgrade any already-installed formulae
 brew upgrade
-
 
 # GNU core utilities (those that come with OS X are outdated)
 brew install coreutils
@@ -21,9 +21,7 @@ brew install gnu-sed --with-default-names
 # Bash 4
 # Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before running `chsh`.
 brew install bash
-
 brew install bash-completion
-
 brew install homebrew/completions/brew-cask-completion
 
 # Install wget with IRI support
@@ -32,12 +30,6 @@ brew install wget --with-iri
 # Install more recent versions of some OS X tools
 brew install vim --with-override-system-vi
 brew install ripgrep
-brew install homebrew/dupes/grep
-brew install homebrew/dupes/openssh
-brew install homebrew/dupes/screen
-
-# z hopping around folders
-brew install z
 
 # Install other useful binaries
 brew install ffmpeg --with-libvpx
@@ -54,3 +46,5 @@ brew install terminal-notifier
 
 # Remove outdated versions from the cellar
 brew cleanup
+
+npm install -g prettier
