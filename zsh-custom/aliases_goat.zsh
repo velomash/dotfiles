025e966464e60eeb664a474b151796a63ac0d0b9
Adam Trimble <adam.trimble@gmail.com>
2020-01-17 14:14:59 -0800

    add aliases for local servers

--- zsh-custom/aliases_goat.zsh
+++ zsh-custom/aliases_goat.zsh
@@ -1,5 +1,11 @@
+# Editing
 alias cms='cd /Users/adam.trimble/Documents/goat-web-cms; nvim'
-alias flight='cd /Users/adam.trimble/Documents/flightclub.com; nvim'
-alias goat='cd /Users/adam.trimble/Documents/web-main; nvim'
-alias goatstaging='cd /Users/adam.trimble/Documents/web-main; GOATENV_ENVOY_EGRESS=https://staging.goat.com npm start'
-alias sneakers='cd /Users/adam.trimble/Documents/sneakers; nvim'
+alias f='cd /Users/adam.trimble/Documents/flightclub.com; nvim'
+alias g='cd /Users/adam.trimble/Documents/web-main; nvim'
+alias s='cd /Users/adam.trimble/Documents/sneakers; nvim'
+
+# Local Servers
+alias scms='cd /Users/adam.trimble/Documents/goat-web-cms/; npm start'
+alias sg='cd /Users/adam.trimble/Documents/web-main; GOATENV_ENVOY_EGRESS=https://staging.goat.com npm start'
+alias sgl='cd /Users/adam.trimble/Documents/web-main; npm start'
+alias ss='cd /Users/adam.trimble/Documents/sneakers/; bin/rails server'
