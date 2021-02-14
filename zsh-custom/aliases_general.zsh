3d31b7ea8fffe9146d1842c34398b146fc87390b
Adam Trimble <adam.trimble@gmail.com>
2021-02-14 12:57:42 -0800

    update config

--- zsh-custom/aliases_general.zsh
+++ zsh-custom/aliases_general.zsh
@@ -1,3 +1,5 @@
-alias v='nvim'
-alias gb='git checkout $(git branch --sort=-committerdate | fzf)'
 alias d='cd /Users/adam.trimble/Documents/dotfiles; nvim'
+alias gb='git checkout $(git branch --sort=-committerdate | fzf)'
+alias gcm='git checkout main'
+alias gpr='gh pr checkout'
+alias v='nvim'
