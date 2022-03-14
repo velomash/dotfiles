8b14e625d7ea497798094f47b5073a81381ec93f
Adam Trimble <adam.trimble@Adams-MacBook-Pro.local>
2022-03-14 16:13:56 -0700

    start setting up lua configs

---
+++ nvim/lua/setup/nerdtree.lua
@@ -0,0 +1,8 @@
+
+" NERDTree options
+noremap <leader>t<leader> :NERDTreeToggle<CR>
+noremap <leader>to :NERDTreeFind<CR>
+noremap <leader>tc :NERDTreeClose<CR>
+noremap <leader>tf :NERDTreeFocus<CR>
+noremap <leader>tr :NERDTreeRefreshRoot<CR>
+let g:NERDTreeWinSize = 50
---
+++ nvim/lua/setup/test.lua
@@ -0,0 +1,5 @@
+
+" vim-test
+let test#strategy = 'neovim'
+let test#neovim#term_position = "vertical"
+let test#javascript#jest#options = '--watch'
