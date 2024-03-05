8d76eba020d3f0be64e0f803edfd99d9ad6a8a96
Adam Trimble <adam.trimble@goat.com>
2024-03-04 22:57:58 -0800

    fixed(): Completion with nvim-cmp

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -14,16 +14,16 @@ packer.init({
 packer.startup(function()
   use 'wbthomason/packer.nvim' -- packer can manage itself
   use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-  use 'github/copilot.vim'
   use 'janko/vim-test'                      -- granular testing
   use 'jiangmiao/auto-pairs'                -- auto close brackets
   use {'junegunn/fzf', run = './install --all'}
   use 'junegunn/fzf.vim'                    -- fuzzy finding with ag
   use 'neovim/nvim-lspconfig'               -- language server
-  use ({'ms-jpq/coq_nvim', branch = 'coq'})  -- completion
-  use ({'ms-jpq/coq.artifacts', branch = 'artifacts'})  -- completion
-  use ({'ms-jpq/coq.thirdparty', branch = '3p'})  -- completion
-  use 'anott03/nvim-lspinstall'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline'
+  use 'hrsh7th/nvim-cmp'
   use {'prettier/vim-prettier', run = 'yarn install' }
   use 'scrooloose/nerdcommenter'            -- easy commenting
   use 'scrooloose/nerdtree'                 -- find files by dir tree
@@ -57,27 +57,3 @@ vim.cmd([[
   command! -bang -nargs=* FindCurrentWord call fzf#vim#grep('rg --column --line-number --no-heading --fixed-strings --ignore-case --hidden --follow --color "always" '.shellescape(expand('<cword>')), 1, <bang>0)
   set grepprg=rg\ --vimgrep
 ]])
-
--- LSP Setup
-vim.cmd([[
-  let g:coq_settings = { 'auto_start': 'shut-up' }
-]])
-local lspconfig = require'lspconfig'
-local completion = require'coq'
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics, {
-    underline = true,
-    virtual_text = true,
-    signs = true,
-    update_in_insert = true,
-  }
-)
-local signs = { Error = "❗️", Warn = "⚠️", Hint = "💡", Info = "💡" }
-for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
--- setup language servers here
-lspconfig.tsserver.setup(coq.lsp_ensure_capabilities({
-  root_dir = require('lspconfig').util.root_pattern("package.json", "tsconfig.json", "jsconfig.json")
-}))
