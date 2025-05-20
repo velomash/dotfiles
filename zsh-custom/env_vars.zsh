7b8589ee0ae061893649d8e69ef82b16f106e57e
Adam Trimble <adam.trimble@goat.com>
2025-05-20 12:11:16 -0700

    added(): updated configs
    improved go support

--- zsh-custom/env_vars.zsh
+++ zsh-custom/env_vars.zsh
@@ -5,12 +5,15 @@ get_pw () {
   security find-generic-password -ga "$1" -w
 }
 export NPM_TOKEN="$(get_pw packagecloud.io)"
+export ANTHROPIC_API_KEY="$(get_pw anthropic_api_key)"
+export ALIAS_API_TOKEN="$(get_pw alias_api_key)"
+export ALIAS_API_TOKEN_STAGING="$(get_pw alias_api_key_staging)"
 
 #   ------------------------------
 #   GOAT ENV VARS
 #   ------------------------------
 export GH_PACKAGES_TOKEN="$(get_pw GOAT_github)"
-export GOPRIVATE="github.com/goatapp"
+export GOPRIVATE="github.com/goatapp/*"
 
 #   ---------------------------
 #   FZF config for use with vim
